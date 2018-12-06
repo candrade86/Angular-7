a4c0e79e88a152c89b8eb1eb7e1777a3bfa82295
Carlos Andrade <c.andrade86@ymail.com>
2018-12-06 16:55:26 -0500

    Added a button.

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // selector: '[app-server] attribute selector <div app-server></div>
+  // selector: '.app-server' class selector <div class="app-server"></div>
   selector: 'app-servers',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
   }
